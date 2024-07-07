f8d4098fa8d267eea5b25b77b373d12119503d05
Thyago <chatack.tc@gmail.com>
2024-07-06 22:35:51 -0300

    :sparkles: [feat] - adiciona componentes de listagem de produtos product-list.tsx, product-item.tsx

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -5,7 +5,7 @@ import { MenuIcon } from "lucide-react";
 const Header = () => {
   return (
     <div className="flex justify-between px-5 pt-6">
-      <Image src="/ifood-logo.png" alt="iFood Logo" height={30} width={100} />
+      <Image src="/ifood-logo.svg" alt="iFood Logo" height={30} width={100} />
       <Button
         size="icon"
         variant="outline"
