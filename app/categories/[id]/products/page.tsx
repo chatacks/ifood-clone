e80563e7a84e42d152849d20baefe3fa1e5d6e3e
Thyago <chatack.tc@gmail.com>
2024-07-08 20:35:19 -0300

    :sparkles: [feat] - adiciona pagina de produtos de categorias

---
+++ app/categories/[id]/products/page.tsx
@@ -0,0 +1,53 @@
+import Header from "@/app/components/header";
+import ProductItem from "@/app/components/product-item";
+import { db } from "@/app/lib/prisma";
+import { notFound } from "next/navigation";
+
+type CategoriesPageProps = {
+  params: {
+    id: string;
+  };
+};
+
+const CategoriesPage = async ({ params: { id } }: CategoriesPageProps) => {
+  const category = await db.category.findUnique({
+    where: {
+      id,
+    },
+    include: {
+      Product: {
+        include: {
+          restaurant: {
+            select: {
+              name: true,
+            },
+          },
+        },
+      },
+    },
+  });
+
+  if (!category) {
+    return notFound();
+  }
+
+  return (
+    <>
+      <Header />
+      <div className="px-5 py-6">
+        <h2 className="mb-6 text-lg font-semibold">{category.name}</h2>
+        <div className="flex grid w-full grid-cols-2 flex-col gap-6">
+          {category.Product.map((product) => (
+            <ProductItem
+              key={product.id}
+              product={product}
+              className="min-w-full"
+            />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CategoriesPage;
