dd0536dc83c322e4d74a283040d99bc27cd90488
Thyago <chatack.tc@gmail.com>
2024-07-09 17:54:54 -0300

    :sparkles: [feat] - adiciona componentes de carrinho e item de carrinho

---
+++ app/context/cart.tsx
@@ -0,0 +1,163 @@
+/* eslint-disable no-unused-vars */
+"use client";
+
+import { Prisma, Product } from "@prisma/client";
+import { createContext, ReactNode, useMemo, useState } from "react";
+import { calculateProductTotalPrice } from "../helpers/price";
+
+export interface CartProduct
+  extends Prisma.ProductGetPayload<{
+    include: {
+      restaurant: {
+        select: {
+          deliveryFee: true;
+        };
+      };
+    };
+  }> {
+  quantity: number;
+}
+
+interface ICartContext {
+  products: CartProduct[];
+  subTotalPrice: number;
+  totalPrice: number;
+  totalDiscounts: number;
+  addProductsToCart: (
+    product: Prisma.ProductGetPayload<{
+      include: {
+        restaurant: {
+          select: {
+            deliveryFee: true;
+          };
+        };
+      };
+    }>,
+    quantity: number,
+  ) => void;
+  decreaseProductQuantity: (productId: string) => void;
+  increaseProductQuantity: (productId: string) => void;
+  removeProductFromCart: (productId: string) => void;
+}
+
+export const CartContext = createContext<ICartContext>({
+  products: [],
+  subTotalPrice: 0,
+  totalPrice: 0,
+  totalDiscounts: 0,
+  addProductsToCart: () => {},
+  decreaseProductQuantity: () => {},
+  increaseProductQuantity: () => {},
+  removeProductFromCart: () => {},
+});
+
+export const CartProvider = ({ children }: { children: ReactNode }) => {
+  const [products, setProducts] = useState<CartProduct[]>([]);
+
+  const subTotalPrice = useMemo(() => {
+    return products.reduce((acc, product) => {
+      return acc + Number(product.price) * product.quantity;
+    }, 0);
+  }, [products]);
+
+  const totalPrice = useMemo(() => {
+    return products.reduce((acc, product) => {
+      return acc + calculateProductTotalPrice(product) * product.quantity;
+    }, 0);
+  }, [products]);
+
+  const totalDiscounts = subTotalPrice - totalPrice;
+
+  const decreaseProductQuantity = (productId: string) => {
+    return setProducts((prevState) =>
+      prevState.map((cartProduct) => {
+        if (cartProduct.id === productId) {
+          if (cartProduct.quantity === 1) {
+            return cartProduct;
+          }
+          return {
+            ...cartProduct,
+            quantity: cartProduct.quantity - 1,
+          };
+        }
+
+        return cartProduct;
+      }),
+    );
+  };
+
+  const increaseProductQuantity = (productId: string) => {
+    return setProducts((prevState) =>
+      prevState.map((cartProduct) => {
+        if (cartProduct.id === productId) {
+          return {
+            ...cartProduct,
+            quantity: cartProduct.quantity + 1,
+          };
+        }
+
+        return cartProduct;
+      }),
+    );
+  };
+
+  const removeProductFromCart = (productId: string) => {
+    return setProducts((prevState) =>
+      prevState.filter((cartProduct) => cartProduct.id !== productId),
+    );
+  };
+
+  const addProductsToCart = (
+    product: Prisma.ProductGetPayload<{
+      include: {
+        restaurant: {
+          select: {
+            deliveryFee: true;
+          };
+        };
+      };
+    }>,
+    quantity: number,
+  ) => {
+    const isProductAlreadyOnCart = products.some(
+      (cartProduct) => cartProduct.id === product.id,
+    );
+
+    if (isProductAlreadyOnCart) {
+      return setProducts((prevState) =>
+        prevState.map((cartProduct) => {
+          if (cartProduct.id === product.id) {
+            return {
+              ...cartProduct,
+              quantity: cartProduct.quantity + quantity,
+            };
+          }
+
+          return cartProduct;
+        }),
+      );
+    }
+
+    setProducts(
+      (prevState) =>
+        [...prevState, { ...product, quantity: quantity }] as CartProduct[],
+    );
+  };
+
+  return (
+    <CartContext.Provider
+      value={{
+        products,
+        subTotalPrice,
+        totalPrice,
+        totalDiscounts,
+        addProductsToCart,
+        decreaseProductQuantity,
+        increaseProductQuantity,
+        removeProductFromCart,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
--- app/products/components/product-details.tsx
+++ app/products/components/product-details.tsx
@@ -1,9 +1,17 @@
 "use client";
 
+import Cart from "@/app/components/cart";
 import DeliveryInfo from "@/app/components/delivery-info";
 import DiscountBadge from "@/app/components/discount-badge";
 import ProductList from "@/app/components/product-list";
 import { Button } from "@/app/components/ui/button";
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+} from "@/app/components/ui/sheet";
+import { CartContext } from "@/app/context/cart";
 import {
   calculateProductTotalPrice,
   formatCurrency,
@@ -11,7 +19,7 @@ import {
 import { Prisma } from "@prisma/client";
 import { ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
 import Image from "next/image";
-import { useState } from "react";
+import { useContext, useState } from "react";
 
 type ProductDetailsProps = {
   product: Prisma.ProductGetPayload<{
@@ -31,6 +39,13 @@ const ProductDetails = ({
   complementaryProducts,
 }: ProductDetailsProps) => {
   const [quantity, setQuantity] = useState(1);
+  const [isCartOpen, setIsCartOpen] = useState(false);
+  const { addProductsToCart } = useContext(CartContext);
+
+  const handleAddToCartClick = () => {
+    addProductsToCart(product, quantity);
+    setIsCartOpen(true);
+  };
 
   const handleIncreaseQuantityClick = () =>
     setQuantity((prevState) => prevState + 1);
@@ -42,81 +57,92 @@ const ProductDetails = ({
     });
 
   return (
-    <div className="relative z-50 mt-[-1.5rem] rounded-tl-2xl rounded-tr-2xl bg-white py-5">
-      <div className="flex items-center gap-[0.375rem] px-5">
-        <div className="relative h-6 w-6">
-          <Image
-            src={product.restaurant.imageUrl}
-            alt={product.restaurant.name}
-            className="rounded-full object-cover"
-            fill
-          />
+    <>
+      <div className="relative z-50 mt-[-1.5rem] rounded-tl-2xl rounded-tr-2xl bg-white py-5">
+        <div className="flex items-center gap-[0.375rem] px-5">
+          <div className="relative h-6 w-6">
+            <Image
+              src={product.restaurant.imageUrl}
+              alt={product.restaurant.name}
+              className="rounded-full object-cover"
+              fill
+            />
+          </div>
+          <span className="text-xs text-muted-foreground">
+            {product.restaurant.name}
+          </span>
         </div>
-        <span className="text-xs text-muted-foreground">
-          {product.restaurant.name}
-        </span>
-      </div>
 
-      <h1 className="mb-3 mt-1 px-5 text-xl font-semibold">{product.name}</h1>
+        <h1 className="mb-3 mt-1 px-5 text-xl font-semibold">{product.name}</h1>
 
-      <div className="flex justify-between px-5">
-        <div>
-          <div className="flex items-center gap-2">
-            <h2 className="text-xl font-semibold">
-              {formatCurrency(calculateProductTotalPrice(product))}
-            </h2>
+        <div className="flex justify-between px-5">
+          <div>
+            <div className="flex items-center gap-2">
+              <h2 className="text-xl font-semibold">
+                {formatCurrency(calculateProductTotalPrice(product))}
+              </h2>
+              {product.discountPercentage > 0 && (
+                <DiscountBadge product={product} />
+              )}
+            </div>
+
             {product.discountPercentage > 0 && (
-              <DiscountBadge product={product} />
+              <p className="text-sm text-muted-foreground">
+                De: {formatCurrency(Number(product.price))}
+              </p>
             )}
           </div>
 
-          {product.discountPercentage > 0 && (
-            <p className="text-sm text-muted-foreground">
-              De: {formatCurrency(Number(product.price))}
-            </p>
-          )}
+          <div className="flex items-center gap-3 text-center">
+            <Button
+              size="icon"
+              variant="ghost"
+              className="border border-solid border-muted-foreground"
+              onClick={handleDecreaseQuantityClick}
+            >
+              <ChevronLeftIcon />
+            </Button>
+            <span className="w-4">{quantity}</span>
+            <Button size="icon" onClick={handleIncreaseQuantityClick}>
+              <ChevronRightIcon />
+            </Button>
+          </div>
         </div>
 
-        <div className="flex items-center gap-3 text-center">
+        <div className="px-5">
+          <DeliveryInfo restaurant={product.restaurant} />
+        </div>
+
+        <div className="mt-6 space-y-3 px-5">
+          <h3 className="font-semibold">Sobre</h3>
+          <p className="text-sm text-muted-foreground">{product.description}</p>
+        </div>
+
+        <div className="mt-6 space-y-3">
+          <h3 className="px-5 font-semibold">Sucos</h3>
+          <ProductList products={complementaryProducts} />
+        </div>
+
+        <div className="px-5">
           <Button
-            size="icon"
-            variant="ghost"
-            className="border border-solid border-muted-foreground"
-            onClick={handleDecreaseQuantityClick}
+            onClick={handleAddToCartClick}
+            size="default"
+            className="mt-6 w-full font-semibold"
           >
-            <ChevronLeftIcon />
-          </Button>
-          <span className="w-4">{quantity}</span>
-          <Button
-            size="icon"
-            className="border border-solid border-muted-foreground"
-            onClick={handleIncreaseQuantityClick}
-          >
-            <ChevronRightIcon />
+            Adicionar ao carrinho
           </Button>
         </div>
       </div>
 
-      <div className="px-5">
-        <DeliveryInfo restaurant={product.restaurant} />
-      </div>
-
-      <div className="mt-6 space-y-3 px-5">
-        <h3 className="font-semibold">Sobre</h3>
-        <p className="text-sm text-muted-foreground">{product.description}</p>
-      </div>
-
-      <div className="mt-6 space-y-3">
-        <h3 className="px-5 font-semibold">Sucos</h3>
-        <ProductList products={complementaryProducts} />
-      </div>
-
-      <div className="px-5">
-        <Button size="default" className="mt-6 w-full font-semibold">
-          Adicionar ao carrinho
-        </Button>
-      </div>
-    </div>
+      <Sheet open={isCartOpen} onOpenChange={setIsCartOpen}>
+        <SheetContent className="w-[90vw]">
+          <SheetHeader>
+            <SheetTitle className="text-left">Sacola</SheetTitle>
+          </SheetHeader>
+          <Cart />
+        </SheetContent>
+      </Sheet>
+    </>
   );
 };
 
