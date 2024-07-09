32ea7278879c0b569d3e541a65aad02091c6179a
Thyago <chatack.tc@gmail.com>
2024-07-09 19:54:46 -0300

    :boom: [fix] - corrige lógica de descontos de pedidos

--- app/context/cart.tsx
+++ app/context/cart.tsx
@@ -69,11 +69,12 @@ export const CartProvider = ({ children }: { children: ReactNode }) => {
     return (
       products.reduce((acc, product) => {
         return acc + calculateProductTotalPrice(product) * product.quantity;
-      }, 0) - Number(products[0]?.restaurant.deliveryFee)
+      }, 0) + Number(products[0]?.restaurant.deliveryFee)
     );
   }, [products]);
 
-  const totalDiscounts = subTotalPrice - totalPrice;
+  const totalDiscounts =
+    subTotalPrice - totalPrice + Number(products[0]?.restaurant.deliveryFee);
 
   const decreaseProductQuantity = (productId: string) => {
     return setProducts((prevState) =>
